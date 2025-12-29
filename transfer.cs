b26e9543ed103e1c332923696eb9445d6af9b7dc
Noor Fatima <2023-bs-cs-265@tuf.edu.pk>
2025-12-29 22:13:34 +0500

    Create transfer.cs

---
+++ transfer.cs
@@ -0,0 +1,23 @@
+private void btnSend_Click(object sender, EventArgs e)
+{
+    decimal amount = Convert.ToDecimal(txtAmount.Text);
+    var con = DB.GetConnection();
+    con.Open();
+
+    string deduct = "UPDATE users SET balance = balance - @a WHERE phone=@p";
+    MySqlCommand cmd1 = new MySqlCommand(deduct, con);
+    cmd1.Parameters.AddWithValue("@a", amount);
+    cmd1.Parameters.AddWithValue("@p", LoginForm.LoggedUserPhone);
+    cmd1.ExecuteNonQuery();
+
+    string insert = "INSERT INTO transactions(user_phone,type,details,amount) " +
+                    "VALUES(@p,'Money Transfer',@d,@a)";
+    MySqlCommand cmd2 = new MySqlCommand(insert, con);
+    cmd2.Parameters.AddWithValue("@p", LoginForm.LoggedUserPhone);
+    cmd2.Parameters.AddWithValue("@d", txtReceiver.Text);
+    cmd2.Parameters.AddWithValue("@a", amount);
+    cmd2.ExecuteNonQuery();
+
+    con.Close();
+    MessageBox.Show("Money Transferred!");
+}
