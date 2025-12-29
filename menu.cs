fe0fb11c494246a55caf5a449c3ef75d87b3db5d
Noor Fatima <2023-bs-cs-265@tuf.edu.pk>
2025-12-29 22:15:28 +0500

    Create menu.cs

---
+++ menu.cs
@@ -0,0 +1,14 @@
+private void btnMobileLoad_Click(object sender, EventArgs e)
+{
+    new MobileLoadForm().Show();
+}
+
+private void btnTransfer_Click(object sender, EventArgs e)
+{
+    new MoneyTransferForm().Show();
+}
+
+private void btnBills_Click(object sender, EventArgs e)
+{
+    new BillPaymentForm().Show();
+}
